711dac67a5c64caa4900022e96f6b5270e79dd07
Bhanu Jain <bhanujain88@gmail.com>
2017-11-26 18:31:36 -0500

    Adding Presentation

--- miners/GoogleMiner.py
+++ miners/GoogleMiner.py
@@ -5,8 +5,9 @@ from bs4 import BeautifulSoup
 class GoogleMiner:
 
     def get_search_summary(self,search_query):
+        search_result_limit = 100
         search_query = search_query.replace(" ", "+")
-        query = "https://www.google.com/search?q=" + search_query
+        query = "https://www.google.com/search?q=" + search_query + "&num=" + str(search_result_limit)
         r = requests.get(query)
         html_doc = r.text
         soup = BeautifulSoup(html_doc, 'html.parser')
