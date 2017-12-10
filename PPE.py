e0cb724c7d3fa709c4fdee9ea7dc6d3bd232aeba
Bhanu Jain <bhanujain88@gmail.com>
2017-12-10 14:47:06 -0500

    Adding Experiments

--- PPE.py
+++ PPE.py
@@ -1,18 +1,29 @@
+import csv
+import glob
 import json
 import string
-from collections import defaultdict
+import sys
+from collections import defaultdict, Counter
+import itertools
 
 import nltk
 import numpy as np
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords, wordnet
+from nltk.sentiment import SentimentIntensityAnalyzer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
 from textblob import TextBlob
 from textblob.en.sentiments import NaiveBayesAnalyzer
 
 from miners.GoogleMiner import GoogleMiner
 from miners.NewsMiner import NewsMiner
 from miners.TwitterMiner import TwitterMiner
+from sklearn import metrics
 
 
 def get_tweets():
@@ -26,6 +37,7 @@ def get_articles():
     with open('out/nyt_articles.json', 'w') as fout:
         json.dump(articles, fout)
     print(articles)
+    return articles
 
     # cnn = NewsMiner().sources['cnn']
     # for i in range(10):
@@ -61,6 +73,34 @@ def stem_tokens(tokens):
     count = nltk.Counter(stemmed)
     # print(count.most_common(10))
     return stemmed
+
+
+def get_article_token(portion):
+    article_token_dict = {}
+    toker = nltk.RegexpTokenizer(r'\w+')
+    with open("out/nyt_articles.json") as data_file:
+        data = json.load(data_file)
+    for article in data:
+        text = article[portion]
+        lowers = text.lower()
+        no_punctuation = lowers.translate(str.maketrans("", "", string.punctuation))
+        tokens = toker.tokenize(no_punctuation)
+        article_token_dict[article['_id']] = " ".join(tokens)
+    return article_token_dict
+
+
+def get_article_token(portion):
+    article_token_dict = {}
+    toker = nltk.RegexpTokenizer(r'\w+')
+    with open("out/nyt_articles.json") as data_file:
+        data = json.load(data_file)
+    for article in data:
+        text = article[portion]
+        lowers = text.lower()
+        no_punctuation = lowers.translate(str.maketrans("", "", string.punctuation))
+        tokens = toker.tokenize(no_punctuation)
+        article_token_dict[article['_id']] = " ".join(tokens)
+    return article_token_dict
 
 
 def get_article_token(portion):
@@ -149,8 +189,19 @@ def get_article_promise_progress(articles, promises, tfidf_matrix, nb=False):
             "result": article_sentiment})
     return progress
 
+def save_google_results(promises, num=10):
+    data = defaultdict(lambda: [])
+    gm = GoogleMiner()
+    for i, promise in enumerate(promises):
+        for s in gm.get_search_summary(promise, num):
+            text = s.text
+            tokens = tokenize(text)
+            data[promise].append(" ".join(remove_stop_words(tokens)))
+    with open('out/google_test_data.json', 'w') as fout:
+            json.dump(data, fout)
+    return data
 
-def get_google_results(num=10):
+def get_google_results(promises, num=10):
     data = defaultdict(lambda: [])
     gm = GoogleMiner()
     for i, promise in enumerate(promises):
@@ -176,30 +227,236 @@ def get_google_promise_progress(google_sum, nb):
     return progress
 
 
-if __name__ == "__main__":
+def experiment_1():
     promises = get_promise_token()
     articles = get_article_token("text")
     articles_sum = get_article_token("summary")
     tfidf_matrix = get_tfidf_matrix(articles, promises)
-    google_sum = get_google_results(10)
-
-
+    google_sum = get_google_results(promises,10)
     results = {'article_text_nb': get_article_promise_progress(articles, promises, tfidf_matrix, nb=True),
                'article_text_pattern': get_article_promise_progress(articles, promises, tfidf_matrix, nb=False),
                'google_nb': get_google_promise_progress(google_sum, nb=True),
                'google_pattern': get_google_promise_progress(google_sum, nb=False)}
-
     tfidf_matrix_sum = get_tfidf_matrix(articles_sum, promises)
     results["article_summary_pattern"] = get_article_promise_progress(articles_sum, promises, tfidf_matrix_sum,
                                                                       nb=False)
     results["article_summary_nb"] = get_article_promise_progress(articles_sum, promises, tfidf_matrix_sum, nb=True)
-
     with open('out/results.json', 'w') as fout:
         json.dump(results, fout, indent=4)
 
-    # with open('out/results.json', 'r') as df:
-    #     res = json.load(df)
-    #
-    # polarity = []
-    # for r in res['article_summary_nb']:
-    #     polarity.append(r['polarity'])
+
+def get_combined_articles():
+    articles = []
+    csv.field_size_limit(sys.maxsize)
+    toker = nltk.RegexpTokenizer(r'\w+')
+    for filename in glob.glob("data/all-the-news/*.csv"):
+        with open(filename) as data_file:
+            news_reader = csv.reader(data_file, delimiter=',', quotechar='"')
+            next(news_reader, None)
+            for row in news_reader:
+                text = row[9]
+                lowers = text.lower()
+                no_punctuation = lowers.translate(str.maketrans("", "", string.punctuation))
+                tokens = toker.tokenize(no_punctuation)
+                articles.append({
+                    "id": row[1],
+                    "publication": row[3],
+                    "text": " ".join(tokens)
+                })
+    return articles
+
+
+def save_articles_with_sentiment():
+    articles = get_combined_articles()
+    sid = SentimentIntensityAnalyzer()
+    for i, article in enumerate(articles):
+        print(i)
+        ss = sid.polarity_scores(article['text'])
+        article["polarity"] = ss
+    with open('out/articles_test_data_with_sentiment.json', 'w') as fout:
+        json.dump(articles, fout)
+
+
+def get_progress_words():
+    progressive_words = ['progress', 'advance', 'breakthrough', 'development', 'evolution', 'growth', 'headway',
+                         'improvement', 'increase', 'momentum', 'movement', 'pace', 'process', 'rise', 'stride',
+                         'mature', 'change', 'enhancement', 'gain', 'upgrade', 'rise']
+    synonyms = set()
+    antonyms = set()
+    for w in progressive_words:
+        for syn in wordnet.synsets(w):
+            for l in syn.lemmas():
+                if "_" not in l.name():
+                    synonyms.add(l.name())
+                if l.antonyms():
+                    antonyms.add(l.antonyms()[0].name())
+    return synonyms, antonyms
+
+
+def get_progressive_counts(text, progress_syn, progress_ant):
+    tokens = Counter(text.split())
+    syn_count = 0
+    ant_count = 0
+    total_count = 0
+    for token, count in tokens.items():
+        total_count += count
+        if token in progress_syn:
+            syn_count += count
+        if token in progress_ant:
+            ant_count += count
+    return syn_count, ant_count, total_count
+
+
+def tanh(x, derivative=False):
+    if derivative:
+        return 1 - (x ** 2)
+    return np.tanh(x)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def generate_articles_test_data():
+    progress_syn, progress_ant = get_progress_words()
+    print(progress_syn, progress_ant)
+    pos_label = 0
+    neg_label = 0
+    with open('out/articles_test_data_with_sentiment.json', 'r') as fin:
+        data = json.load(fin)
+        for i, rec in enumerate(data):
+            syn_count, ant_count, total_count = get_progressive_counts(rec['text'], progress_syn, progress_ant)
+            if total_count <= 0:
+                continue
+            polarity = rec['polarity']['compound']
+            # print(rec['text'])
+            pp = syn_count / total_count
+            pn = ant_count / total_count
+            ppn = tanh(pp) - tanh(pn)
+            progress = 1 if ppn >= 0.01 else 0
+            label = 1 if polarity * progress > 0 else 0
+            if label == 1:
+                pos_label += 1
+            else:
+                neg_label += 1
+            # print(
+            #     "row=%s,polarity=%s,syn_count=%s, ant_count=%s, total_count=%s, pp=%s, pn=%s, ppn=%s, progress=%s label=%s" % (i,polarity, syn_count, ant_count, total_count, pp, pn, ppn, progress, label))
+            rec['label'] = label
+            rec['progress'] = progress
+            rec['pos_progress'] = pp
+            rec['neg_progress'] = pn
+            rec['norm_progress'] = ppn
+        with open('out/articles_test_data.json', 'w') as fout:
+            json.dump(data, fout)
+        print("pos_label=%s; neg_label=%s" % (pos_label, neg_label))
+
+
+def get_train_articles():
+    articles = []
+    labels = []
+    with open('out/articles_test_data.json', 'r') as fin:
+        data = json.load(fin)
+        for i, rec in enumerate(data):
+            if 'text' in rec and 'label' in rec:
+                articles.append(rec['text'])
+                labels.append(rec['label'])
+    return articles, labels
+
+
+def experiment_2():
+    # nltk.download('vader_lexicon')
+    promises = get_promise_token()
+    articles, labels = get_train_articles()
+
+    docs_train, docs_test, y_train, y_test = train_test_split(
+        articles, labels, test_size=0.25, random_state=None)
+
+    # TASK: Build a vectorizer / classifier pipeline that filters out tokens
+    # that are too rare or too frequent
+    pipeline = Pipeline([
+        ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
+        ('clf', MultinomialNB()),
+    ])
+
+    # TASK: Build a grid search to find out whether unigrams or bigrams are
+    # more useful.
+    # Fit the pipeline on the training set using grid search for the parameters
+    parameters = {
+        'vect__ngram_range': [(1, 1), (1, 2)],
+    }
+    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1)
+    grid_search.fit(docs_train, y_train)
+
+    # TASK: print the mean and std for each candidate along with the parameter
+    # settings for all the candidates explored by grid search.
+    n_candidates = len(grid_search.cv_results_['params'])
+    for i in range(n_candidates):
+        print(i, 'params - %s; mean - %0.2f; std - %0.2f'
+              % (grid_search.cv_results_['params'][i],
+                 grid_search.cv_results_['mean_test_score'][i],
+                 grid_search.cv_results_['std_test_score'][i]))
+
+    # TASK: Predict the outcome on the testing set and store it in a variable
+    # named y_predicted
+    y_predicted = grid_search.predict(docs_test)
+
+    # Print the classification report
+    print(metrics.classification_report(y_test, y_predicted,
+                                        target_names=["No Progress", "Progress"]))
+
+    # Print and plot the confusion matrix
+    cm = metrics.confusion_matrix(y_test, y_predicted)
+    print(cm)
+
+
+def get_test_articles(promises):
+    articles = []
+    test_articles = get_article_token("text")
+    tfidf_matrix = get_tfidf_matrix(test_articles, promises)
+    for i, promise in enumerate(promises):
+        cosine_sim = cosine_similarity(tfidf_matrix[i: i + 1], tfidf_matrix)
+        single_array = np.array(cosine_sim[0])
+        article_array = single_array.argsort()[-6:][::-1]
+        matched_articles = [s for s in article_array if s > 1]
+        for x in matched_articles:
+            articles.append(test_articles[list(test_articles.keys())[x - 2]])
+    return articles
+
+
+def experiment_3():
+    promises = get_promise_token()
+    # test_articles = get_test_articles(promises)
+    test_articles = list(get_article_token("text").values())
+    train_articles, train_labels = get_train_articles()
+    pipeline = Pipeline([
+        ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
+        ('clf', MultinomialNB()),
+    ])
+    pipeline.fit(train_articles, train_labels)
+    y_predicted = pipeline.predict(test_articles)
+    for i,a in enumerate(test_articles):
+        print(a)
+        print(y_predicted[i])
+
+
+def experiment_4():
+    promises = get_promise_token()
+    test_data = list(itertools.chain.from_iterable(get_google_results(promises,10).values()))
+    train_articles, train_labels = get_train_articles()
+    pipeline = Pipeline([
+        ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
+        ('clf', MultinomialNB()),
+    ])
+    pipeline.fit(train_articles, train_labels)
+    y_predicted = pipeline.predict(test_data)
+    for i, a in enumerate(test_data):
+        print(a)
+        print(y_predicted[i])
+
+
+if __name__ == "__main__":
+    # experiment_1()
+    # generate_articles_test_data()
+    # experiment_2()
+    experiment_3()
+    # experiment_4()
--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # political-promise-evaluation
 PPE provides a unified view of promises made by Political Candidates while campaign- ing and the promises fulfilled during their tenure. PPE aims to scour Political Manifesto’s and summarize political promises. We will extract relevant Tweets on Twitter and then evaluate the the fulfillment status of the promises
+
+
+# Experiments
+
+## Experiment 2
+
+```
+    0 params - {'vect__ngram_range': (1, 1)}; mean - 0.82; std - 0.00
+    1 params - {'vect__ngram_range': (1, 2)}; mean - 0.81; std - 0.00
+                 precision    recall  f1-score   support
+    
+    No Progress       0.83      0.99      0.90     28942
+       Progress       0.65      0.10      0.18      6677
+    
+    avg / total       0.79      0.82      0.76     35619
+    
+    [[28575   367]
+     [ 5994   683]]
+```
+ 
+## Experiment 3
+
+
+
+## Experiment 4
+
+Identified all summaries to zero.
+
+`results/experiment4.txt`
+
+# Report
+
+
+## Data
+
+### Promises
+- Trump Tracker API 
+
+### Promise Evaluation
+- Twiiter 
+- NYT
+- Google Search
+
+## Methodology
+
+### Document Matching 
+- Bag of Words
+- Tf-Idf
+
+### Progress Tracking
+- Naive Bayes (NTLK - Pre trained) (Pure Sentiment)
+- Pattern (Lexical) (Pure Sentiment)
+- Custom Trained Classifier (Vader Sentiment + Bag of words for Progressive Words )
+    - Train Data Labelling
+        - Label = 0 (Broken or in Progress)
+        - Label = 1 (Completed)
+        - Label = Binary(Binary(progress_coefficient) * polarity)
+            - Polarity = vader polarity
+            - progress_coefficient, 
+                ```
+                    progress_synonyms = syn_count / total_count
+                    progress_antonyms = ant_count / total_count
+                    progress_coefficient  = tanh(pp) - tanh(pn)
+                ```
+    - Naive Bayes -> Experiment 2
+    - Random Forest (TODO)
+    - SVM (TODO)
+    
+    
